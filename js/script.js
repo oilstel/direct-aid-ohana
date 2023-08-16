c70ad9b29d23050c2988d008821d6f93ad4fcdfe
Elliott Cost <elliottcost@Elliotts-MacBook-Air.local>
2023-08-16 17:16:11 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-fetch('ohana_py/ohana.json')
-.then(response => response.json())
-.then(data => {
-    // The data might be nested since there could be multiple tables
-    const flattenedData = data.flat();
+function getRandomFamily() {
+    fetch('ohana_py/ohana.json')
+    .then(response => response.json())
+    .then(data => {
+        const flattenedData = data.flat();
 
+        const randomItem = flattenedData[Math.floor(Math.random() * flattenedData.length)];
 
-    // Randomly select an item
-    const randomItem = flattenedData[Math.floor(Math.random() * flattenedData.length)];
+        console.log(flattenedData);
 
-    console.log(flattenedData);
+        document.getElementById('family-name').textContent = randomItem.family_name;
+        document.getElementById('donation-link').innerHTML = randomItem.donation_link;
+        document.getElementById('description').textContent = randomItem.description;
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
+}
 
-    // Update the HTML with the selected item
-    document.getElementById('family-name').textContent = randomItem.family_name;
-    document.getElementById('donation-link').innerHTML = randomItem.donation_link;
-    document.getElementById('description').textContent = randomItem.description;
-})
-.catch(error => {
-    console.error("Error fetching data:", error);
-});
+document.getElementById('support').addEventListener('click', getRandomFamily);
